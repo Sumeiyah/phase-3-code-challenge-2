74e9d5dcc7258fe41130ef5d091e64dee555a452
Sumeya Farah <136057548+Sumeiyah@users.noreply.github.com>
2023-08-30 12:09:15 +0300

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -14,14 +14,6 @@ review1 = Review(customer1, restaurant1, 4)
 review2 = Review(customer2, restaurant1, 5)
 review3 = Review(customer1, restaurant2, 3)
 
-    
-
-
-    
-
-
-
-
 # Print information
 print(customer1.full_name())
 print(restaurant1.name())
